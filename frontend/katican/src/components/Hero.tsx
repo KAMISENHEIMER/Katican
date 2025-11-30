9ddd61b37ed3417a8e73809c12cc52393064f70d
KAMISENHEIMER <therealspydude@gmail.com>
2025-11-30 14:40:40 -0600

    created Katican hero

---
+++ frontend/katican/src/components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Hero.css';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-dust"></div>
+      
+      <span className="welcome-line">WELCOME TO THE</span>
+      <h1 className="main-title">KATICAN</h1>
+      <h2 className="sub-title">A collaborative creative writing project.</h2>
+
+    </section>
+  );
+};
+
+export default Hero;
