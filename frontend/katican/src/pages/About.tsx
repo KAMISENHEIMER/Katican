612c9a6f5dfec7d815a44e32a12f87c71468a450
KAMISENHEIMER <therealspydude@gmail.com>
2025-12-02 19:35:02 -0600

    added temp about page

--- frontend/katican/src/pages/About.tsx
+++ frontend/katican/src/pages/About.tsx
@@ -1,10 +1,56 @@
 import React from 'react';
-
+import Navbar from '../components/Navbar';
 
 const About = () => {
   return (
     <div>
-      About WIP
+      <Navbar />
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          margin: 0,
+          color: "white",
+          backgroundImage: "url(https://www.transparenttextures.com/patterns/stardust.png), linear-gradient(180deg, #170d3dff, #040812ff)",
+        }}
+      >
+        <h1
+          style={{
+            fontFamily: '"Cinzel", serif',
+            fontWeight: 500,
+            margin: "10px",
+            fontSize: "3rem",
+          }}
+        >
+          About
+        </h1>
+
+        <div
+          style={{
+            fontFamily: '"Montserrat", sans-serif',
+            color: "#cccccc",
+            fontSize: "1.1rem",
+            marginBottom: "10px",
+          }}
+        >
+          Page under construction
+        </div>
+
+        <div
+          style={{
+            fontFamily: '"Cormorant Garamond", serif',
+            color: "#d4af37",
+            fontSize: "1.4rem",
+            letterSpacing: "1px",
+          }}
+        >
+          I am but a man
+        </div>
+      </div>
     </div>
   );
 };
